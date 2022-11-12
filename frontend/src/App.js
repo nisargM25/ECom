82032996771814af973c524997dbd91d219caa60
nisargM25 <nisarggateway25@gmail.com>
2022-11-12 10:20:57 +0530

    forth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Navbar, Container } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
 import Products from "./pages/Products";
 import Add from "./pages/Add";
 import Update from "./pages/Update";
@@ -9,50 +7,23 @@ import VRegister from "./pages/VRegister";
 import VLogin from "./pages/VLogin";
 import { useContext } from "react";
 import { AuthContext } from "./context/authContext";
+import Home from "./pages/Home";
 
 function App() {
-  const { currentUser, vlogout } = useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
 
   return (
     <div className="App">
       <BrowserRouter>
-        <div className="d-flex flex-column site-container">
-          <header className="App-header">
-            <Navbar bg="dark" variant="dark" >
-
-              <Container>
-               <LinkContainer to={currentUser ? `/Home` : `/`}>
-                  <Navbar.Brand>E-mart</Navbar.Brand>
-                </LinkContainer>
-
-                {/* {currentUser ? <LinkContainer to="/Home">
-                  <Navbar.Brand>E-mart</Navbar.Brand>
-                </LinkContainer> : <LinkContainer to="/">
-                  <Navbar.Brand>E-mart</Navbar.Brand>
-                </LinkContainer>} */}
-                {currentUser && <LinkContainer to="/add">
-                  <Navbar.Brand>Add Product</Navbar.Brand>
-                </LinkContainer>}
-              </Container>
-              <div className="UserName">
-                <span></span>
-                {currentUser && <span className="logoutUser" onClick={vlogout}>Logout {currentUser?.v_name}</span>}
-              </div>
-            </Navbar>
-          </header>
-          <main>
+          <div>
             <Routes>
-              <Route path="/Home" element={currentUser?<Products />:<VLogin />} />
+              <Route path="/Home" element={currentUser?<Home />:<VLogin />} />
               <Route path="/VRegister" element={!currentUser?<VRegister />:<Products/>} />
               <Route path="/" element={!currentUser?<VLogin />:<Products/>} />
               <Route path="/add" element={currentUser?<Add />:<VLogin />} />
               <Route path="/update/:id" element={currentUser?<Update />:<VLogin />} />
             </Routes>
-          </main>
-          <footer>
-            <div className="text-center">All rights reserved</div>
-          </footer>
-        </div>
+          </div>
       </BrowserRouter>
     </div>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,26 +1,43 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Container, Navbar } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import Products from './Products'
+import { AuthContext } from '../context/authContext'
 
 const Home = () => {
+  const { currentUser, vlogout } = useContext(AuthContext);
   return (
     <div>
-        <header className="App-header">
-          <Navbar bg="dark" variant="dark" >
-            <Container>
-              <LinkContainer to="/">
-                <Navbar.Brand>E-mart</Navbar.Brand>
-              </LinkContainer>
-              <LinkContainer to="/add">
-                <Navbar.Brand>Add Product</Navbar.Brand>
-              </LinkContainer>
+        <div className="d-flex flex-column site-container">
+          <header className="App-header">
+            <Navbar bg="dark" variant="dark" >
+
+              <Container>
+               <LinkContainer to="/Home">
+                  <Navbar.Brand>E-mart</Navbar.Brand>
+                </LinkContainer>
+
+                {currentUser && <LinkContainer to="/add">
+                  <Navbar.Brand>Add Product</Navbar.Brand>
+                </LinkContainer>}
+              </Container>
               
-            </Container>
-          </Navbar>
-        </header>
-        
+              <div className="UserName">
+                <span></span>
+                {currentUser && <span className="logoutUser" onClick={vlogout}>Logout {currentUser?.v_name}</span>}
+              </div>
+            </Navbar>
+          </header>
+          <main>
+            <Products />
+          </main>
+          <footer>
+            <div className="text-center">All rights reserved</div>
+          </footer>
+        </div>
+      
     </div>
   )
 }
 
-export default Home
+export default Home;
